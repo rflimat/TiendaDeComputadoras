a29447d656c905ef85a7994174bad6f6dce85ebf
rflimat <rflimat@users.noreply.github.com>
2021-02-27 16:48:09 -0500

    Clases usuario mejorado

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,21 +1,26 @@
 #include<iostream>
 #include<string.h>
-#include "Usuario.cpp"
 using namespace std;
 
-class Cliente : public Usuario{
+class Cliente : public Usuario {
     private:
         string codigo;
         string preferencias;
     public:
-        Cliente(string,string,string,string,int,string,string,string,string,string);
+        Cliente(string,string,string,string,int,string,string,string,int,string);
+        bool confirmar_acceso(string,string);
         void modificar_cliente(int);
         void mostrar_cliente();
 };
 
-Cliente::Cliente(string u,string c,string n,string a,int e,string d,string t,string m,string cd,string p) : Usuario(u,c,n,a,e,d,t,m){
-    codigo=c;
+Cliente::Cliente(string u,string c,string n,string a,int e,string d,string t,string m,int cd,string p) : Usuario(u,c,n,a,e,d,t,m){
+    codigo="C-00"+IntToString(cd);
     preferencias=p;
+}
+
+bool Cliente::confirmar_acceso(string u,string c){
+    if((get_usuario()==u)&&(get_contrasenia()==c))
+        return true;
 }
 
 void Cliente::modificar_cliente(int dato){
@@ -29,7 +34,8 @@ void Cliente::modificar_cliente(int dato){
 }
 
 void Cliente::mostrar_cliente(){
+    cout<<left;
+    cout<<setw(25)<<codigo;
     mostrar_usuario();
-    cout<<"Codigo de cliente: "<<codigo<<endl;
-    cout<<"Preferencias: \n"<<preferencias<<endl;
+    cout<<setw(25)<<preferencias;
 }
