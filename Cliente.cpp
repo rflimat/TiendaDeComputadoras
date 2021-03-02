45595924ac50db9817ce349cf4479206bfc8dc58
rflimat <rflimat@users.noreply.github.com>
2021-03-01 19:17:49 -0500

    Clase Cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -8,14 +8,29 @@ class Cliente : public Usuario {
         string preferencias;
     public:
         Cliente(string,string,string,string,int,string,string,string,int,string);
+        string get_codigo();
+        string get_nombres();
+        string get_DNI();
         bool confirmar_acceso(string,string);
         void modificar_cliente(int);
         void mostrar_cliente();
 };
 
 Cliente::Cliente(string u,string c,string n,string a,int e,string d,string t,string m,int cd,string p) : Usuario(u,c,n,a,e,d,t,m){
-    codigo="C-00"+IntToString(cd);
+    codigo="C-00"+IntToString(cd+1);
     preferencias=p;
+}
+
+string Cliente::get_codigo(){
+    return codigo;
+}
+
+string Cliente::get_nombres(){ 
+    return Usuario::get_nombres();;
+}
+
+string Cliente::get_DNI(){
+    return Usuario::get_DNI();
 }
 
 bool Cliente::confirmar_acceso(string u,string c){
@@ -28,6 +43,7 @@ void Cliente::modificar_cliente(int dato){
         modificar_usuario(dato);
     }
     if((dato==9)||(dato==0)){
+        fflush(stdin);
         cout<<"Cambiar preferencias: ";
         getline(cin,preferencias);
     }
@@ -35,7 +51,7 @@ void Cliente::modificar_cliente(int dato){
 
 void Cliente::mostrar_cliente(){
     cout<<left;
-    cout<<setw(25)<<codigo;
+    cout<<setw(8)<<codigo;
     mostrar_usuario();
-    cout<<setw(25)<<preferencias;
+    cout<<setw(16)<<preferencias;
 }
